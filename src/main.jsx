c63e5a048f4eabe2d099c5885225c13c5ad26dcb
Shogo Tasai <tasai@serverworks.co.jp>
2025-01-03 14:37:41 +0900

    componentのexport形式をdefault式ではなく個別に明示exportする方式に変更

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import LearningLog from './LearningLog.jsx'
+import { LearningLog } from './LearningLog.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
