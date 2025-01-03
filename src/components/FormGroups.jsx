c64b60c1e3432635be4b540bc653fe3ef200664d
Shogo Tasai <tasai@serverworks.co.jp>
2025-01-03 20:54:09 +0900

    動的に複数生成される要素に対してkey属性を追加

--- src/components/FormGroups.jsx
+++ src/components/FormGroups.jsx
@@ -20,7 +20,7 @@ export const FormGroups = ({ items }) => {
     return (
         <div>
             {items.map(({ label, key, type, placeholder, value, onChange }) => (
-                <div style={styles.formGroup}>
+                <div style={styles.formGroup} key={key}>
                     <label style={styles.label} htmlFor={key}>{label}</label>
                     <input style={styles.input} id={key} type={type} placeholder={placeholder} value={value} onChange={onChange} />
                 </div>
--- src/components/PreviewList.jsx
+++ src/components/PreviewList.jsx
@@ -2,7 +2,7 @@ export const PreviewList = ({ items }) => {
     return (
         <div>
             {items.map(({ label, value }) => (
-                <p>入力されている{label}：{value}</p>
+                <p key={label}>入力されている{label}：{value}</p>
             ))}
         </div>
     )
