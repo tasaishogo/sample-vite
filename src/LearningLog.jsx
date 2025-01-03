77f4811b1d43e92c575d0c00de690b3b968b36a0
Shogo Tasai <tasai@serverworks.co.jp>
2025-01-03 16:04:07 +0900

    学習入力欄のcomponentを作成

--- src/LearningLog.jsx
+++ src/LearningLog.jsx
@@ -1,8 +1,20 @@
 import { useState } from 'react'
+import { InputLogs } from './components/InputLogs'
 import { LogList } from './components/LogList'
 
 
 export const LearningLog = () => {
+  const [logTitle, setLogTitle] = useState('')
+  const [logTime, setLogTime] = useState(0)
+
+  const onChangeLogTitle = (e) => {
+    setLogTitle(e.target.value)
+  }
+
+  const onChangeLogTime = (e) => {
+    setLogTime(e.target.value)
+  }
+
   const records = [
     { title: "勉強の記録1", time: 1 },
     { title: "勉強の記録2", time: 3 },
@@ -11,6 +23,12 @@ export const LearningLog = () => {
   return (
     <>
       <h1>学習記録</h1>
+      <InputLogs
+        logTitle={logTitle}
+        logTime={logTime}
+        onChangeLogTitle={onChangeLogTitle}
+        onChangeLogTime={onChangeLogTime}
+      />
       <LogList logs={records} />
     </>
   )
